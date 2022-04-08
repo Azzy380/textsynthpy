00e2a62122d4af58df6bf7d6f3ebe192bf3c3e16
Azzy380 <59232769+Azzy380@users.noreply.github.com>
2022-04-08 23:18:40 +0200

    Add files via upload

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open(path.join(HERE, 'README.md'), encoding='utf-8') as f:
 # This call to setup() does all the work
 setup(
     name="textsynthpy",
-    version="0.2.5",
+    version="0.2.8",
     description="Demo textsynth api wrapper. You need textsynth.com account to get api key. Issues: https://github.com/Azzy380/textsynthpy/issues",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -36,5 +36,5 @@ setup(
     ],
     packages=["textsynthpy"],
     include_package_data=True,
-    install_requires=["requests", "json"]
+    install_requires=["requests"]
 )
